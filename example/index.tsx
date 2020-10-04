c651e94baf2877b1c18913307b0ca8127784aabc
birving2 <brandon@peakactivity.com>
2020-10-04 01:55:53 -0400

    added example 3

--- example/index.tsx
+++ example/index.tsx
@@ -2,39 +2,40 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ContextStateProvider, useContextState } from '../.';
-const exampleConfig ={
+const exampleConfig = {
   first: 'Megaman', last: 'isTheBest'
 }
-const exampleConfig2 ={
+const exampleConfig2 = {
   count: 0
 }
-const exampleConfig3 ={
+const exampleConfig3 = {
   pokemonInfo: 0,
-  isLoadingPokemonInfo: true
+  isLoadingPokemonInfo: false
 }
-const globalFunctions =(props: any)=>{
-  async function getPokemonInfo(){
-    let pokemonInfo = {error: true}
-    try{
-      const data =  await fetch('https://pokeapi.co/api/v2/pokemon/greninja')
-      const greninjaInfo= await data.json()
-      pokemonInfo = greninjaInfo
-    }catch(e){
+const globalFunctions = (props: any) => {
+  async function getPokemonInfo(name: string) {
+    let pokemonInfo = { error: true }
+    props.updateContextState({ isLoadingPokemonInfo: true })
+
+    try {
+      const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLocaleLowerCase()}`)
+      pokemonInfo = await data.json()
+    } catch (e) {
       console.error('There was an error', e)
     }
-    props.updateContextState({pokemonInfo, isLoadingPokemonInfo: false})
+    props.updateContextState({ pokemonInfo, isLoadingPokemonInfo: false })
   }
-return {
-  getPokemonInfo
-}
+  return {
+    getPokemonInfo
+  }
 }
 const App = () => {
   return (
     <div>
-      <ContextStateProvider  stateConfig={exampleConfig}>
+      <ContextStateProvider stateConfig={exampleConfig}>
         <Example />
       </ContextStateProvider>
-      <ContextStateProvider  stateConfig={exampleConfig2}>
+      <ContextStateProvider stateConfig={exampleConfig2}>
         <Example2 />
       </ContextStateProvider>
       <ContextStateProvider globalFunctions={globalFunctions} stateConfig={exampleConfig3}>
@@ -44,21 +45,21 @@ const App = () => {
   );
 };
 const Example = () => {
-  const {first, last, updateContextState} = useContextState()
+  const { first, last, updateContextState } = useContextState()
 
-  function handleChange(key, e:any){
-    updateContextState({[key]: e.target.value})
+  function handleChange(key, e: any) {
+    updateContextState({ [key]: e.target.value })
   }
-  return(
+  return (
     <div>
-       <input type="text" id="first" onChange={(e:any)=>handleChange('first',e)} value={first} />
-       <input type="text" id="last" onChange={(e:any)=>handleChange('last',e)} value={last} />
+      <input type="text" id="first" onChange={(e: any) => handleChange('first', e)} value={first} />
+      <input type="text" id="last" onChange={(e: any) => handleChange('last', e)} value={last} />
     </div>
   )
 }
 const Example2 = () => {
   const { count } = useContextState()
-  return(
+  return (
     <div>
       {count}
     </div>
@@ -66,21 +67,32 @@ const Example2 = () => {
 }
 const Example3 = () => {
   const { pokemonInfo, isLoadingPokemonInfo, getPokemonInfo } = useContextState()
-  React.useEffect(()=>{
-    getPokemonInfo()
-  },[])
-  if(isLoadingPokemonInfo){
+  const [pokemonName, setPokemonName] = React.useState('')
+  async function handleClick() {
+    await getPokemonInfo(pokemonName)
+  }
+  if (isLoadingPokemonInfo) {
     return <div>Loading...</div>
   }
-  if(pokemonInfo.error){
-    return <div>Error loading pokemon info</div>
-  }
-  return(
-    <div style={{display:'flex', alignItems: 'center'}}>
-      <img src={pokemonInfo.sprites.front_shiny} />
-      <span> {pokemonInfo.order}</span>: {pokemonInfo.name}
+  return (
+    <>
+      <div>
+        <input name="pokemonName" onChange={(e: any) => setPokemonName(e.target.value)} value={pokemonName} type="text" />
+        <button type="button" onClick={handleClick}>Search pokemon</button>
+      </div>
+      {
+        pokemonInfo.error && <div>Error finding pokemon</div>
+      }
+      {pokemonInfo.name &&
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={pokemonInfo.sprites.front_shiny} />
+          <span> {pokemonInfo.order}</span>: {pokemonInfo.name}
 
-    </div>
+        </div>
+
+      }
+    </>
+
   )
 }
 ReactDOM.render(<App />, document.getElementById('root'));
